eae7cf5c6938c4ff467ec864585df79c26392e4d
Neja's Mac <neja.cesnik@hotmail.com>
2020-10-19 00:46:32 +0100

    Updates

---
+++ precache-manifest.373f50a3048fb76b931957846270dcc6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c643026d1727c314671bfc1b179a1d4c",
+    "url": "/Virtual-Pub-Quiz-Frontend/index.html"
+  },
+  {
+    "revision": "7a8cffc207cd807b1b3f",
+    "url": "/Virtual-Pub-Quiz-Frontend/static/css/main.9e158f8a.chunk.css"
+  },
+  {
+    "revision": "2b9217a2b615dce029f0",
+    "url": "/Virtual-Pub-Quiz-Frontend/static/js/2.3d1543b3.chunk.js"
+  },
+  {
+    "revision": "ef7f0128066dad638e14e7838f835a83",
+    "url": "/Virtual-Pub-Quiz-Frontend/static/js/2.3d1543b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a8cffc207cd807b1b3f",
+    "url": "/Virtual-Pub-Quiz-Frontend/static/js/main.c30854bd.chunk.js"
+  },
+  {
+    "revision": "9390edc6059245a3e596",
+    "url": "/Virtual-Pub-Quiz-Frontend/static/js/runtime-main.567f6bbc.js"
+  },
+  {
+    "revision": "bee86d1d8d0fa25f32610d7623efa1cd",
+    "url": "/Virtual-Pub-Quiz-Frontend/static/media/LeaderboardFull.bee86d1d.png"
+  },
+  {
+    "revision": "608bd790c044919aaaf2ebf75208389f",
+    "url": "/Virtual-Pub-Quiz-Frontend/static/media/Logo.608bd790.png"
+  },
+  {
+    "revision": "0ea0042b9944f7481d178ce9489496f0",
+    "url": "/Virtual-Pub-Quiz-Frontend/static/media/Ready.0ea0042b.png"
+  }
+]);
